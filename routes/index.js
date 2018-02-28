d3690f84d7b076424a1ee297e13f13eb5816d2de
Nikolas Kohvakka <nikolas.kohvakka@gmail.com>
2018-02-28 17:13:05 +0200

    /generateGroup([names], role, groupName) [NEW]   -Can be used to create multiple users with same group all at once   -Used to create accounts for whole age group   -Users real names are handled as an array   -loginId will automatically be firstname.surname   -Part of UMS
    /deleteParticipant(id, usrId) [NEW]
      -Part of EMS
      -Removes user (user are called "participants" when looking from event's perspective) from event and adding next user in reserve to participants.
      -Reserve = users, that haven't fit in but are in-line ("backup attenders")
      -Participants = users, that are in and attending.
    
    /dbSearch(onlyMeta, value) [NEW]
      -Used to search whole database
      -Client search bar uses this function
      -Automatically filters unauthorized data away.
    
    /getSettings() [NEW]
      -Added new mongodb collection called "globalsettings"
      -getSettings returns systems global settings, if user requesting them is an admin
      -Used to control for example how many events each role should complete
    
    /changeSettings (settings) [NEW]
      -sets globalsettings
    
    /changeSettings
    
    /joinEvent
      -Added support for reserved participants.
      -When max ammount of participants has been reached, joining user will automatically be added to reserved participants.
    
    /login
      -Added backend support for changePass -argument.
    
    /createUser
      -New userdata variable "reservedEvents" for in-line (reserved) events

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,16 @@ router.get('/', function(req, res, next) {
   res.send(200);
 });
 
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 8; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
 router.get('/info', function(req, res) {
   if(req.session.loggedIn) {
     var db = req.db;
@@ -39,9 +49,27 @@ function encrypt(text){
   return crypted;
 }
 
-router.get('/login', function(req, res) { //REMEBER TO DO THINGS WITH THE changePass!
+router.get('/login', function(req, res) {
   var db = req.db;
   var collection = db.get('usercollection');
+  if(req.query.changePass == "true") {
+    bcrypt.compare(req.query.oldPass, req.session.usr.pass, function(err, cor) {
+      if(cor) {
+        bcrypt.hash(req.query.newPass, saltRounds, function(err, hash) {
+          if(err) res.send("There was an error while hashing: " + err);
+          else {
+              collection.update({ _id: req.session.usr._id }, {$set: { pass: hash, changePass: false }});
+              req.session.usr.changePass = false;
+              res.send(req.session);
+          }
+        })
+      }
+      else {
+        res.send(403)
+      }
+    })
+  }
+  else {
   collection.findOne({ loginId: req.query.id },{},function(e,docs){
     if(e) {
       res.send(e);
@@ -53,12 +81,13 @@ router.get('/login', function(req, res) { //REMEBER TO DO THINGS WITH THE change
         req.session.usr = docs;
         }
         else {
-         res.status(430);
+         res.status(403);
          req.session.loggedIn = false;
         }
         res.send(req.session);
       });
   });
+}
 });
 
 router.get('/logout', function(req, res) {
@@ -73,7 +102,7 @@ router.get('/createUser', function(req, res) {
     bcrypt.hash(req.query.pass, saltRounds, function(err, hash) {
       if(err) res.send("There was an error while hashing: " + err);
       else {
-        collection.insert({name: req.query.name, role: req.query.role, pass: hash, changePass: true, eventRequirements: req.query.eventRequirements, loginId: req.query.loginId, verifiedEvents: JSON.stringify(new Array), events: JSON.stringify(new Array)},function(e,docs){
+        collection.insert({name: req.query.name, role: req.query.role, pass: hash, changePass: true, eventRequirements: req.query.eventRequirements, loginId: req.query.loginId, verifiedEvents: JSON.stringify(new Array), events: JSON.stringify(new Array), reservedEvents: JSON.stringify(new Array)},function(e,docs){
           res.send("OK");
         })
       }
@@ -82,12 +111,94 @@ router.get('/createUser', function(req, res) {
   else res.send(403)
 });
 
+router.get('/generateGroup', function(req, res) {
+  if(req.session.loggedIn && req.session.usr.role == "admin"){
+    console.log(req.query)
+    var db = req.db;
+    var collection = db.get('usercollection');
+    req.query.names = JSON.parse(req.query.names)
+    var profileCache = new Array();
+    req.query.names.forEach(function(name, index){
+      var pass = makeid();
+      var settings = db.get("globalsettings")
+      settings.findOne({}, function(e,docs2){
+      if(e) console.log(e)
+      var eventRequirements = docs2.settings.eventRequirements[req.query.role]
+      bcrypt.hash(pass, saltRounds, function(err, hash) {
+        if(err) res.send("There was an error while hashing: " + err);
+        else {
+          collection.insert({name: name, role: req.query.role, groupId: req.query.groupName, pass: hash, changePass: true, eventRequirements: eventRequirements, loginId: name.replace(" ", "."), verifiedEvents: JSON.stringify(new Array), events: JSON.stringify(new Array), reservedEvents: JSON.stringify(new Array)},function(e,docs){
+            profileCache.push({name: name, pass: pass, loginId: name.replace(" ", ".")});
+            if(profileCache.length == req.query.names.length) res.send(JSON.stringify(profileCache))
+          })
+        }
+      });
+      })
+    })
+  }
+});
+
 router.get('/deleteUser', function(req, res) {
   if(req.session.loggedIn && req.session.usr.role == "admin" && req.query != ""){
     var db = req.db;
     var collection = db.get('usercollection');
       collection.remove({_id: ObjectId(req.query.id)}, {}, function(e, docs){
         res.send("OK");
+      })
+  }
+  else res.send(403)
+});
+
+router.get('/deleteParticipant', function(req, res) {
+  if(req.session.loggedIn && req.session.usr.role == "admin" || req.session.usr._id == req.query.usrId && req.query != ""){
+    var db = req.db;
+    var collection = db.get('eventcollection');
+    var usrCollection = db.get('usercollection');
+      collection.findOne({_id: ObjectId(req.query.id)}, {}, function(e, docs){
+        var docsCache = docs
+        if(e) console.log(e)
+        else {
+          usrCollection.findOne({ _id: req.query.usrId },{},function(e,removedUserData){
+              if(e) {
+                console.log(e)
+                res.send("That user doesn't exist or the database is down")
+              }
+            var removeUserEvents = JSON.parse(removedUserData.events)
+            var index3 = removeUserEvents.indexOf(req.query.id)
+            removeUserEvents.splice(index3, 1)
+            var index = JSON.parse(docsCache.participants).indexOf(req.query.usrId)
+            docsCache = JSON.parse(docsCache.participants)
+            reservedParticipantsCache = JSON.parse(docs.reservedParticipants);
+            docsCache.splice(index,1)
+            if(reservedParticipantsCache.length > 0) {
+                usrCollection.findOne({ _id: reservedParticipantsCache[0] },{},function(e,usrDocs){
+                if(e) {
+                  console.log(e)
+                  res.send("That user doesn't exist or the database is down")
+                }
+                console.log(usrDocs)
+                docsCache.push(usrDocs._id)
+                var reservedEventsCache = JSON.parse(usrDocs.reservedEvents);
+                var index2 = reservedEventsCache.indexOf(req.query.id);
+                reservedEventsCache.splice(index2, 1)
+                var eventsCache = JSON.parse(usrDocs.events)
+                eventsCache.push(req.query.id)
+                console.log(reservedParticipantsCache.splice(0,1))
+                collection.update({ _id: req.query.id }, {$set: { participants: JSON.stringify(docsCache) }});
+                collection.update({ _id: req.query.id }, {$set: { reservedParticipants: JSON.stringify(reservedParticipantsCache.splice(0,1)) }});
+                usrCollection.update({ _id: usrDocs._id }, {$set: { reservedEvents: JSON.stringify(reservedEventsCache) }});
+                usrCollection.update({ _id: usrDocs._id }, {$set: { events: JSON.stringify(eventsCache) }});
+                usrCollection.update({ _id: req.query.usrId }, {$set: { events: JSON.stringify(removeUserEvents) }});
+                res.send("removed using reserve");
+                })
+            }
+            else {
+              collection.update({ _id: req.query.id }, {$set: { participants: JSON.stringify(docsCache) }});
+              usrCollection.update({ _id: req.query.usrId }, {$set: { events: JSON.stringify(removeUserEvents) }});
+              res.send("removed without reserve");
+            }
+        })
+        }
       })
   }
   else res.send(403)
@@ -153,8 +264,7 @@ router.get('/joinEvent', function(req, res) {
     var db = req.db;
     var collection = db.get('eventcollection');
     collection.findOne({ _id: req.query.id },{},function(e,docs){
-      if(JSON.parse(docs.participants).length < docs.max || docs.max == 0) {
-        var save = JSON.parse(docs.participants);
+       var save = JSON.parse(docs.participants);
         var fresh = true;
         save.forEach(function(entry){
           if(entry == req.query.usrId) {
@@ -162,6 +272,7 @@ router.get('/joinEvent', function(req, res) {
           }
         })
         if(fresh) {
+        if(JSON.parse(docs.participants).length < docs.max || docs.max == 0) {
           var done = false;
           var usrCollection = db.get('usercollection');
           usrCollection.findOne({ _id: req.query.usrId },{},function(e,usrDocs){
@@ -248,12 +359,34 @@ router.get('/joinEvent', function(req, res) {
           });
         }
         else {
-          res.send("You have already joined!")
+          var save2 = JSON.parse(docs.reservedParticipants);
+          var fresh2 = true;
+          save2.forEach(function(entry){
+            if(entry == req.query.usrId) {
+              fresh2 = false;
+            }
+          })
+          if(fresh2) {
+            var reservedParticipantsCache = JSON.parse(docs.reservedParticipants)
+            reservedParticipantsCache.push(req.query.usrId)
+            var usrCollection = db.get('usercollection');
+            usrCollection.findOne({ _id: req.query.usrId },{},function(e,usrDocs){
+              if(e) {
+                res.send("That user doesn't exist or the database is down")
+              }
+              reservedEventsCache = JSON.parse(usrDocs.reservedEvents)
+              reservedEventsCache.push(req.query.id);
+              collection.update({ _id: req.query.id }, {$set: { reservedParticipants: JSON.stringify(reservedParticipantsCache) }});
+              usrCollection.update({ _id: req.query.usrId }, {$set: { reservedEvents: JSON.stringify(reservedEventsCache) }});
+              res.send("You have reserved position!")
+          })
         }
+        else res.send("You have already joined!")
+      }
       }
       else {
-        res.send("This event is already full!")
-      }
+          res.send("You have already joined!")
+        }
     });
   }
   else res.send(403)
@@ -282,9 +415,14 @@ router.get('/getEvents', function(req, res) {
       temp.forEach(function(val, index){
       val.closes = new Date(val.closes.replace("GMT ", "GMT+"))
        if(val.closes > controlDate) {
-         if(JSON.parse(val.participants).length < val.max || val.max == 0) {
+        console.log(req.session.usr.events.includes(val._id))
+        console.log(val._id)
+        if(!req.session.usr.events.includes(val._id))
+        {
+       //  if(JSON.parse(val.participants).length < val.max || val.max == 0) {
           joinableCache.push(val)
-         } 
+        // } 
+        }
        }
       })
       temp = joinableCache
@@ -307,11 +445,114 @@ router.get('/searchEvents', function(req, res) {
   }
 })
 
+router.get('/dbSearch', function(req, res) {
+  if(req.session.loggedIn){
+    var groupCache = new Array();
+    var response = new Array();
+    var db = req.db;
+    var data = false
+    var eventcollection = db.get('eventcollection');
+    var usercollection = db.get('usercollection');
+    eventcollection.find({}, function(e,eventdocs){
+          eventdocs = eventdocs.map(function(val){
+          if(_.lowerCase(val.name).includes(_.lowerCase(req.query.term))) return val
+        })
+        usercollection.find({}, function(e,userdocs){
+        userdocs = userdocs.map(function(val){
+          if(_.lowerCase(val.name).includes(_.lowerCase(req.query.term)) || _.lowerCase(val.role).includes(_.lowerCase(req.query.term))) return val
+        })
+        userdocs = _.compact(userdocs)
+        eventdocs = _.compact(eventdocs)
+        usercollection.find({}, function(e,groupArray){
+          groupArray.forEach(function(user, index){
+            if(_.lowerCase(user.groupId).includes(_.lowerCase(req.query.term))) {
+              groupCache.push(user)
+            }
+            if(index + 1 == groupArray.length) {
+              if(groupCache.length > 0 && req.session.usr.role == "admin") {
+                if(!req.query.onlyMeta == "true") {
+                  groupCache.push(groupCache[0].groupId)
+                  if(response.length == 0) response=groupCache
+                  else response.concat(groupCache)
+                }
+                else {
+                  if(response.length == 0) response = [{type: "group", id: groupCache[0].groupId, name: groupCache[0].groupId}]
+                  else response.concat([{type: "group", id: groupCache[0].groupId, name: groupCache[0].groupId}])
+                }
+                data=true
+              }
+              if(eventdocs.length > 0) {
+                if(!req.query.onlyMeta == "true") {
+                  if(response.length == 0) response=eventdocs
+                  else response.concat(eventdocs)
+                }
+                else {
+                  if(response.length == 0) response = _.compact(eventdocs.map(function(event){
+                    if(req.session.usr.role == "innostaja" && event.toInnostaja == "true") {
+                      return{type: "event", id: event._id, name: event.name}
+                    }
+                    if(req.session.usr.role == "EVI" && event.toEVI == "true") {
+                      return{type: "event", id: event._id, name: event.name}
+                    }
+                    if(req.session.usr.role == "admin") {
+                      return{type: "event", id: event._id, name: event.name}
+                    }
+                  }))
+                  else response.concat(eventdocs.map(function(event){return {type: "event", id: event._id, name: event.name}}))
+                }
+                data=true
+              }
+              if(userdocs.length > 0 && req.session.usr.role == "admin") {
+                if(!req.query.onlyMeta == "true") {
+                  if(response.length == 0) response=userdocs
+                  else response.concat(userdocs)
+                }
+                else {
+                  if(response.length == 0) response = userdocs.map(function(user){return {type: "user", id: user._id, name: user.name}})
+                  else response.concat(eventdocs.map(function(user){return {type: "user", id: user._id, name: user.name}}))
+                }
+                data=true
+              }
+              if(data) {
+                res.send(response);
+              }
+              else res.send(new Array())
+            }
+          })
+        })
+        })
+    })
+  }
+})
+
+router.get('/getSettings', function(req, res) {
+  if(req.session.loggedIn && req.session.usr.role == "admin" && req.query != "") {
+    var db = req.db;
+    var collection = db.get('globalsettings');
+      collection.findOne({}, function(e,docs){
+        if(e) console.log(e)
+        console.log(docs)
+        res.send(docs.settings)
+    })
+  }
+  else res.send(403)
+})
+
+router.get('/changeSettings', function(req, res) {
+  if(req.session.loggedIn && req.session.usr.role == "admin" && req.query != "") {
+    var db = req.db;
+    var collection = db.get('globalsettings');
+      collection.update({}, {$set: { settings: JSON.parse(req.query.settings) }});
+        res.send("OK")
+  }
+  else res.send(403)
+})
+
 router.get('/setEvent', function(req, res) {
   if(req.session.loggedIn && req.session.usr.role == "admin"){
     var db = req.db;
     var collection = db.get('eventcollection');
-    collection.insert({name: req.query.name, startTime: req.query.startTime, endTime: req.query.endTime, closes: req.query.closes, location: req.query.location, toInnostaja: req.query.toInnostaja, toEVI: req.query.toEVI, toIndividual: req.query.toIndividual, max: req.query.max, participants: JSON.stringify(new Array), jobs: req.query.jobs, type: req.query.type},function(e,docs){
+    collection.insert({name: req.query.name, startTime: req.query.startTime, endTime: req.query.endTime, closes: req.query.closes, location: req.query.location, toInnostaja: req.query.toInnostaja, toEVI: req.query.toEVI, toIndividual: req.query.toIndividual, max: req.query.max, participants: JSON.stringify(new Array), reservedParticipants: JSON.stringify(new Array), info: req.query.info, releaseTime: req.query.releaseTime, jobs: req.query.jobs, type: req.query.type},function(e,docs){
         res.send("OK");
     });
   }
